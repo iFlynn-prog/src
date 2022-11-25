d8a287f04e1f6d0bbbc01a0f8e3b4e44ff7c2561
iFlynn-prog <minibikk@gmail.com>
2022-11-25 10:33:00 +0300

    First comment

---
+++ App2.java
@@ -0,0 +1,29 @@
+/*Создайте массив из элементов `0, 1, 3, 4, 5, 2, 1, -4, -1, 10, 55`.
+- Выведите элементы массива в обратном порядке;*/
+
+
+public class App2 {
+    public static void main(String[] args) {       
+       RepeatArr();
+
+    }
+
+         public static void RepeatArr() {
+        int arr[] = new int [] {0, 1, 3, 4, 5, 2, 1, -4, -1, 10, 55};
+            int rezerv[] = new int [11];
+            int temp = 0;
+
+            for (int i = 0; i < arr.length; i++) {
+                               
+                temp = arr[arr.length -1 -i];
+                rezerv[i] = temp;
+                
+                System.out.print(rezerv[i]);
+                
+            }
+
+
+    
+        }
+    
+}
---
+++ App3.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+/*- Добавьте любой элемент в конец массива (грубо говоря, задача в том, чтобы создать новый массив размером на 1 
+    больше предыдущего, загнать в него элементы предыдущего, а дальше присвоить предыдущему массиву ссылку на новый);*/
+
+
+public class App3 {
+    public static void main(String[] args) {
+        AddLastIndexOfArray();   
+    }
+    
+    public static void AddLastIndexOfArray() {
+                            // 0, 1, 2, 3, 4, 5, 6,  7,  8,  9, 10 
+        int arr[] = new int[] {0, 1, 3, 4, 5, 2, 1, -4, -1, 10, 55};
+        int newarr[] = new int[12];
+        Scanner in = new Scanner(System.in);
+        
+
+        for (int i = 0; i < arr.length; i++) {
+            newarr[i] = arr[i];             
+        }
+        
+        System.out.print("Введите число которое отобразится в конце массива" + " ");
+        int number = in.nextInt();
+        in.close();
+        
+        newarr[newarr.length -1] = number;     
+        System.out.print(Arrays.toString(newarr));     
+        
+    }
+
+}
